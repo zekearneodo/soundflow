df430cca9d9732a1ded4a966b5a4962a6560b6bd
Zeke Arneodo <earneodo@ucsd.edu>
2016-10-07 13:16:57 -0700

    zebra finch sound sysnthesis

--- dynamo/finch.py
+++ dynamo/finch.py
@@ -20,7 +20,7 @@ def takens_finch(v, pars):
     ])
 
 
-def finch(pars, int_par_stream, s_f=44100., x_0=None):
+def finch(pars, int_par_stream, x_0=None):
     """
     :param pars: dictionary with parameters, includyng ['sys'] with parameters of the diff equation
     :param int_par_stream: numpy array stream of parameters for integration with columns [alpha, beta, envelope]
@@ -40,7 +40,9 @@ def finch(pars, int_par_stream, s_f=44100., x_0=None):
 
     stream_len = int_par_stream.shape[0]
 
-    steps_per_sample = 20
+    steps_per_sample = pars['sys']['steps_per_sample']
+    s_f = pars['sys']['s_f']
+
     d_t = 1. / (s_f * steps_per_sample)
 
     total_steps = stream_len * steps_per_sample
@@ -76,14 +78,12 @@ def finch(pars, int_par_stream, s_f=44100., x_0=None):
             t_fractional += d_t * steps_per_sample
             sampling_t = steps_per_sample
 
-        pars['sys']['noise'] = np.random.normal()
-        pars['A_1'] = np.sqrt(pars['sys']['envelope']) * (1. + 0.1*pars['sys']['noise'])
-        pars['sys']['beta_1'] = beta_1 * (1. + 0.1 * pars['sys']['noise'])
-
+        pars['sys']['noise'] = np.random.normal(size=2)
+        pars['A_1'] = np.sqrt(pars['sys']['envelope']) * (1. + pars['sys']['noise_fraction_env'] * pars['sys']['noise'][0])
+        pars['sys']['beta_1'] = beta_1 * (1. + pars['sys']['noise_fraction_beta_1'] * pars['sys']['noise'][1])
 
         db_old = tract_buffers[step, db]
-        tract_buffers[step, a] = 0.5 * pars['A_1'] * x[1] + \
-                                 tract_buffers[step - pars['tau_1'], bb]
+        tract_buffers[step, a] = pars['t_in'] * pars['A_1'] * x[1] + tract_buffers[step - pars['tau_1'], bb]
 
         tract_buffers[step, bb] = pars['r_12'] * tract_buffers[step - pars['tau_1'], a] + \
                                   pars['t_21'] * tract_buffers[step - pars['tau_2'], cb]
@@ -128,15 +128,19 @@ def main():
                 'alpha_2': 0.15,
                 'beta_2': 0.15,
                 'gamma': 23500.,
-                'Ch_inv': 4.5E-10,
-                'Lb_inv': 1.1E-4,
-                'Lg_inv': 0.012195,
-                'Rb': 5.0E8,
-                'Rh': 6.0E5,
+                'Ch_inv': 4.5E10,
+                'Lb_inv': 1.E-4,
+                'Lg_inv': 1/82.,
+                'Rb': 5E6,
+                'Rh': 6E5,
                 'V_ext': 0.,
                 'dV_ext': 0.,
                 'noise': 0.,
-                'envelope': 0.
+                'envelope': 0.,
+                'noise_fraction_beta_1': 0.1,
+                'noise_fraction_env': 0.1,
+                's_f': 44100.,
+                'steps_per_sample': 20
                 }
 
     vocal_pars = {'sys': sys_pars,
@@ -155,6 +159,7 @@ def main():
                   't_21': None,
                   't_23': None,
                   't_32': None,
+                  't_in': 0.5,
                   'tau_1': None,
                   'tau_2': None,
                   'tau_3': None,
